9bddb0d81ac08e218a927f9ece4b1218ac7743d4
wltn8078 <wltn8078@naver.com>
2019-11-25 16:48:05 +0900

    1125

--- main/js/main.js
+++ main/js/main.js
@@ -7,35 +7,13 @@ $(window).scroll(function() {
     }
 });
 
+//  1면
 
-$(window).scroll(function(){
-	var scLef = $(this).scrollLeft();
-	if(scLef > 1300) {
-		$("#abme-1").addClass("abme-1"); }
-else{
-		$("#abme-1").removeClass("abme-1");}
-});
-
-
-$(window).scroll(function(){
-	var scTop = $(this).scrollTop();
-	if(scTop > 400) {
-		$(".bt-top").addClass("bt-top-show");}
-else{
-		$(".bt-top").removeClass("bt-top-show");}
-});
-$(".bt-top").click(function(){
-	$("html, body").stop().animate({"scrollTop":0},500);
-});
-
-
-$(".navs").mouseover(function(){
-	$(this).find(".nav-tit").css({"transform": "scale(1.05)"});
-});
-$(".navs").mouseleave(function(){
-	$(this).find(".nav-tit").css({"transform": "scale(1)"});
-});
-
+$(".zees-logo").css({"stroke-dasharray": "2600", "stroke-dashoffset": "2600"})
+$("#zees2-1").addClass('zees-1');
+$("#zees2-2").addClass('zees-2');
+setTimeout(function(){
+$(".hello").css({"opacity": 1})}, 2500);
 
 $(".tags").mouseover(function(){
 	$(this).stop().css({"transform": "translateX(50px)"});
@@ -44,6 +22,70 @@ $(".tags").mouseleave(function(){
 	$(this).stop().css({"transform": "translateX(100px)"});
 });
 
+$(".tag-bar").click(function(){
+	$(".tops").css({"opacity" : 0});
+	$(".main-indexs").css({"opacity" : 0});
+	$(".main-bar").css({"opacity" : 0});
+	$(".zees-logo").css({"stroke-dasharray": "2600", "stroke-dashoffset": "2600"})
+	$("#zees1").removeClass('zees-1-Out');
+	$("#zees2").removeClass('zees-2-Out');
+	$("#zees1").addClass('zees-1');
+	$("#zees2").addClass('zees-2');
+	$(".navis-overlay").stop().css({"transform": "translateX(0%)"});
+	setTimeout(function(){
+		$(".navis-wrap").css({"transform": "translateX(0%)"})},200);
+	$(".navis-overlay, .navis-wrap").on('scroll touchmove mousewheel', function(e){
+		e.preventDefault();
+		e.stopPropagation(); 
+		return false;
+		})
+	});
+	
+	$(".navis-overlay , .close-bt").click(function(){
+		$(".zees-logo").css({"stroke-dasharray": "2600", "stroke-dashoffset": "0"})
+		$("#zees1").removeClass('zees-1');
+		$("#zees2").removeClass('zees-2');
+		$("#zees1").addClass('zees-1-Out');
+		$("#zees2").addClass('zees-2-Out');
+		setTimeout(function(){
+		$(".main-bar").css({"opacity" : 1});
+		$(".navis-wrap").stop().css({"transform": "translateX(100%)"})
+		$(".navis-overlay").css({"transform": "translateX(100%)"})
+		$(".tops").css({"opacity" : 1});
+		$(".main-indexs").css({"opacity" : 1});
+		$(".navis-overlay").off('scroll touchmove mousewheel')},1400);
+});
+
+$(".main-index").mouseover(function(){
+	$(this).css({"color": "rgb(73,0,0)"});
+	$(this).find(".index-line").css({"background-color":"rgb(73,0,0)"});
+})
+$(".main-index").mouseleave(function(){
+	$(this).css({"transform": "translateY(0px)", "color": "#FFF"});
+	$(this).find(".index-line").css({"background-color":"#fff"});
+})
+
+$(".navs").mouseover(function(){
+	$(this).find(".nav-tit").css({"transform": "scale(1.05)"});
+});
+$(".navs").mouseleave(function(){
+	$(this).find(".nav-tit").css({"transform": "scale(1)"});
+});
+
+//  2면
+$(".aboutme2 .me-2 .prog").mouseover(function(){
+	$(this).find("img").css({"transform": "scale(1.1)","	filter": "opacity(1) saturate(0.6)"});
+	$(this).parent().css({"box-shadow" : "1px 1px 4px #ccc"});
+});
+$(".aboutme2 .me-2 .prog").mouseleave(function(){
+	$(this).find("img").css({"transform": "scale(1)","	filter": "opacity(0.8) saturate(0.7)"});
+	$(this).parent().css({"box-shadow" : "none"});
+});
+
+
+
+
+//  3면
 
 $(".work-page").mouseover(function(){
 	$(this).stop().css({"transform": "translateY(-20px)"});
@@ -91,41 +133,6 @@ $(".pager-down").click(function(){
 })
 	
 	
-	$(".tag-bar").click(function(){
-	$(".main1 .logo").css({"opacity" : 0});
-	$(".zees-logo").css({"stroke-dasharray": "2600", "stroke-dashoffset": "2600"})
-	$("#zees1").removeClass('zees-1-Out');
-	$("#zees2").removeClass('zees-2-Out');
-	$("#zees1").addClass('zees-1');
-	$("#zees2").addClass('zees-2');
-	$(".navis-overlay").stop().css({"transform": "translateX(0%)"});
-	$("body").css({ "overflow-y": "hidden"})
-	setTimeout(function(){
-		$(".navis-wrap").css({"transform": "translateX(0%)"})},200);
-	$(".navis-overlay, .navis-wrap").on('scroll touchmove mousewheel', function(e){
-		e.preventDefault();
-		e.stopPropagation(); 
-		return false;
-		})
-	});
-	
-	$(".navis-overlay , .close-bt").click(function(){
-		$(".zees-logo").css({"stroke-dasharray": "2600", "stroke-dashoffset": "0"})
-		$("#zees1").removeClass('zees-1');
-		$("#zees2").removeClass('zees-2');
-		$("#zees1").addClass('zees-1-Out');
-		$("#zees2").addClass('zees-2-Out');
-		setTimeout(function(){
-		$(".navis-wrap").stop().css({"transform": "translateX(100%)"})
-		$(".navis-overlay").css({"transform": "translateX(100%)"})
-		$("body").css({ "overflow-y": "scroll"});
-		$(".main1 .logo").css({"opacity" : 1});
-		$(".navis-overlay").off('scroll touchmove mousewheel')},1400);
-});
-
-
-
-
 
 
 
